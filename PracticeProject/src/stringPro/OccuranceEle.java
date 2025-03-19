928549c9a1253b7f330534b285eee010261ff679
pjagtap8010 <pratikjagtap.qa@gmail.com>
2025-03-19 14:41:46 +0530

    CommiteProject
    test

---
+++ PracticeProject/src/stringPro/OccuranceEle.java
@@ -0,0 +1,47 @@
+package stringPro;
+
+public class OccuranceEle {
+
+	
+	public static void main (String [] args) {
+		
+		
+		String p = "hii pratik how are you hii";
+	//	String [] p1 = p.split(" ");          
+		
+		 String [] p1 = {"p", "pra", "p", "jagtap", "pra"};
+		
+		boolean [] visited = new boolean [p1.length];
+		
+		for(int i=0; i<=p1.length-1; i++) {
+			
+			
+			if(visited[i]) {
+				continue;
+			}
+		
+			 int count =1;
+			for (int j=i+1; j<=p1.length-1; j++) {
+				
+				if(p1[i]==p1[j]) {
+					
+				visited[j] = true;
+				count++;
+				
+				}
+			}
+			
+			
+			
+			System.out.println(p1[i] + "=" + count);
+			
+		}
+		
+		
+		
+		
+		
+	}
+	
+	
+}
