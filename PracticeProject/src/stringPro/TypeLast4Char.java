fb29d455149adf1e153f9b5217d2e2fc77e1f2ea
pjagtap8010 <pratikjagtap.qa@gmail.com>
2025-03-19 16:53:31 +0530

    t
    t

---
+++ PracticeProject/src/stringPro/TypeLast4Char.java
@@ -0,0 +1,18 @@
+package stringPro;
+
+public class TypeLast4Char {
+	
+	public static void main(String[] args) {
+		
+		String s = "hii pratik";
+		
+		String s1 = s.substring(1,4);
+		
+		
+		System.out.println(s1);
+		
+		
+		
+	}
+
+}
